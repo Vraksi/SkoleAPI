764c4b7065b4f9369339a2a66bc18feb12bb7cc5
Jakob Vestergaard <jakobmetalv@gmail.com>
2020-09-01 14:50:49 +0200

    Connection med API

--- MongoAPI/MongoAPI/Startup.cs
+++ MongoAPI/MongoAPI/Startup.cs
@@ -25,7 +25,16 @@ namespace MongoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+           services.AddCors(options =>
+           {
+             options.AddPolicy("CorsPolicy",
+                 builder => builder.AllowAnyOrigin()
+                 .AllowAnyMethod()
+                 .AllowAnyHeader()
+                 .AllowCredentials());
+           });
+           
+           services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,8 +43,8 @@ namespace MongoAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-        app.UseHttpsRedirection();
-      }
+                app.UseHttpsRedirection();
+            }
 
             
 
